03ce121a545ee99a1510299829ef80b6042bf41d
alessandromanfe <alessandro.manfe98@gmail.com>
2025-07-16 16:36:47 +0200

    feat: solution 21 anchor navigation

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,20 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, input } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="href()" [fragment]="anchor()">
       <ng-content />
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLinkWithHref],
 })
 export class NavButtonComponent {
-  href = input('');
+  anchor = input<string>('');
+  href = input<string>('');
 }
