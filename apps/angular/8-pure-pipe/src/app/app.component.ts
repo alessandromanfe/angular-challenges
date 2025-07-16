af7139eda076623a3d7fd4952cc5597e799fd9c2
alessandromanfe <alessandro.manfe98@gmail.com>
2025-07-16 13:13:14 +0200

    feat: solution 8 pipe

--- apps/angular/8-pure-pipe/src/app/app.component.ts
+++ apps/angular/8-pure-pipe/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { HeavyComputationPipe } from './pipe';
 
 @Component({
   selector: 'app-root',
   template: `
     @for (person of persons; track person) {
-      {{ heavyComputation(person, $index) }}
+      {{ person | heavyComputation: $index }}
     }
   `,
+  imports: [HeavyComputationPipe],
 })
 export class AppComponent {
   persons = ['toto', 'jack'];
-
+  /*
   heavyComputation(name: string, index: number) {
     // very heavy computation
     return `${name} - ${index}`;
   }
+  */
 }
